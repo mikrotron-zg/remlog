1be8ba1cc1bc9266589051d3f019dfadc4c5ae7d
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2022-09-07 09:43:39 +0200

    Initialize Spring Boot project

---
+++ src/main/java/hr/mikrotron/remlog/RemlogApplication.java
@@ -0,0 +1,13 @@
+package hr.mikrotron.remlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/mikrotron/remlog/RemlogApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.mikrotron.remlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
